af54144d9d001d992c40f91680e31bae3bfd48e4
Daniel Park <dpeace926@gmail.com>
2017-04-25 22:48:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+
+  var addBump = function() {
+    counter += 1
+  }
+
+  var getBumps = function() {
+    return counter
+  }
+
+  return {addBump, getBumps}
+}
+
+/////////////////////////////////////////////////
+
+function createAnimal(animalType) {
+  function crazyAnimal(deadlyDevice) {
+    return {animalType: animalType, deadlyDevice: deadlyDevice}
+  }
+
+  return crazyAnimal;
+}
+
+
+var sharkCreator = createAnimal("Shark")
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+var sharkWithFrickinCannon = sharkCreator("Cannon")
